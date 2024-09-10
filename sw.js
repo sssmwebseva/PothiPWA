a7066ff89da0c3bf8740ec22497fe29d02e8ad90
sssmwebseva <sssmwebseva@gmail.com>
2024-09-10 15:25:08 +0530

    Trying cache only strategy

--- sw.js
+++ sw.js
@@ -1,18 +1,33 @@
 const CACHE_NAME = `shrikrishna-vijay-v1`;
-/*const CACHE_ASSETS = [
-  'app.html',
-  'app.js',
-  'app.css',
-  'purva.json',
-  'uttar.json',
+const CACHE_ASSETS = [
+  '/app.html',
+  '/app.js',
+  '/app.css',
+  '/purva.json',
+  '/uttar.json',
   '/img/bg.jpg',
   '/img/Swami.jpg'
-];*/
+];
 
 // Use the install event to pre-cache all initial resources.
-/*self.addEventListener('install', e => {
-  console.log('Service Worker: Installed');
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installing');
+  
+  /*Delete any old caches*/
+  e.waitUntil(
+  caches.keys().then(cacheNames => {
+    return Promise.all(
+      cacheNames.map(cache => {
+        //if ( cache !== CACHE_NAME ){ Just clear all caches
+          console.log(`Service Worker: Clearing Old Cache ${cache}`);
+          return caches.delete(cache);
+        //}
+      })
+    )
+  })
+  ).then(() => self.skipWaiting())
 
+ /**/
   e.waitUntil(
     caches.open(CACHE_NAME)
     .then( cache => {
@@ -21,11 +36,15 @@ const CACHE_NAME = `shrikrishna-vijay-v1`;
   })
   .then(() => self.skipWaiting())
  );
-});*/
-
-self.addEventListener('install', e => {
-  console.log('Service Worker: Installed');
 });
+
+/*self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+});*/
+/* 
+Activate event fires when the service worker is activated. So this is going to happen multiple times.
+Every time the app is launched. Whereas install happens only once when the Service Worker is registered
+
 
 self.addEventListener('activate', e => {
 
@@ -45,27 +64,48 @@ e.waitUntil(
   })
 )
 
-})
+})*/
 
-self.addEventListener('fetch', e => {
+/*self.addEventListener('fetch', e => {
   console.log("Service Worker: Fetching");
   //Open the cache first
   e.respondWith(caches.open(CACHE_NAME).then((cache) => {
 	// Go to network first
 	return fetch(e.request).then((fetchedResponse) => {
 		if(fetchedResponse.status == 200){
-		cache.put(e.request, fetchedResponse.clone());
-		return fetchedResponse;}
-		else{
-			// If the network is available however website is down then serve from cache
-			cache.match(e.request).then(cachedResponse => {
-				return cachedResponse;			            
-			});			
+			cache.put(e.request, fetchedResponse.clone());
+			return fetchedResponse;
 		}
+		else{
+			cache.match(e.request).then(cachedResponse => {
+				
+			return cachedResponse;	
+		// If the network is unavailable, get        
+		})
+			
+		}
+
 	}).catch(err => cache.match(e.request).then(cachedResponse => {
 		return cachedResponse;	
 		// If the network is unavailable, get        
 	}));
   }));
 
+});*/
+
+//Cache Only strategy
+self.addEventListener('fetch', (event) => {
+  // Is this one of our precached assets?
+  const url = new URL(event.request.url);
+  const isPrecachedRequest = CACHE_ASSETS.includes(url.pathname);
+
+  if (isPrecachedRequest) {
+    // Grab the precached asset from the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request.url);
+    }));
+  } else {
+    // Go to the network
+    return;
+  }
 });
