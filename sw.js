30709c05adfdfba5072940f61909b8165fa8f3f1
sssmwebseva <sssmwebseva@gmail.com>
2024-09-25 19:02:50 +0530

    Fixing Service Worker Code

--- sw.js
+++ sw.js
@@ -27,36 +27,36 @@ self.addEventListener('install', e => {
 });
 
 //Cache Only strategy
-self.addEventListener('fetch', (event) => {
- 
-  if (caches.has(CACHE_NAME).then((hasCache) => {
+self.addEventListener('fetch', (event) => { 
+  caches
+       .has(CACHE_NAME)
+       .then((hasCache) => {
 
-    if(!hasCache){
-      // Go to the network
-      return;
-    } else {
-          // Is this one of our precached assets?
-      const url = new URL(event.request.url);
-      const isPrecachedRequest = CACHE_ASSETS.includes(url.pathname);
+            if(!hasCache){
+              // Go to the network
+              return;
+            } else {
+                  // Is this one of our precached assets?
+              const url = new URL(event.request.url);
+              const isPrecachedRequest = CACHE_ASSETS.includes(url.pathname);
 
-      if (isPrecachedRequest) {           
-          // Grab the precached asset from the cache
-      
-          event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-            return cache.match(event.request.url);
-          }));
+              if (isPrecachedRequest) {           
+                  // Grab the precached asset from the cache
+              
+                  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+                    return cache.match(event.request.url);
+                  }));
 
-      }else {
-          console.log(`ServiceWorker Fetch: The resource ${event.request.url} is not in cache`);
-          return;
-      }
+              }else {
+                  console.log(`ServiceWorker Fetch: The resource ${event.request.url} is not in cache`);
+                  return;
+              }
 
-    }
-
-
-  }).catch(() => {
-    console.log(`ServiceWorker Fetch: Exception in has`);
-
-  }));
-
+            }
+          }
+        ).catch( () => {
+          console.log(`ServiceWorker Fetch: Exception in has`);
+          }
+        );
+  
 });
