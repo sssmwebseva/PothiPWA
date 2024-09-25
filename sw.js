d7201433979dd0296fe1d236cbad799cf922f811
sssmwebseva <sssmwebseva@gmail.com>
2024-09-25 19:59:13 +0530

    Upgrade is not so easy

--- sw.js
+++ sw.js
@@ -28,35 +28,16 @@ self.addEventListener('install', e => {
 
 //Cache Only strategy
 self.addEventListener('fetch', (event) => { 
-  caches
-       .has(CACHE_NAME)
-       .then((hasCache) => {
-
-            if(!hasCache){
-              // Go to the network
-              return;
-            } else {
-                  // Is this one of our precached assets?
-              const url = new URL(event.request.url);
-              const isPrecachedRequest = CACHE_ASSETS.includes(url.pathname);
-
-              if (isPrecachedRequest) {           
-                  // Grab the precached asset from the cache
-              
-                  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-                    return cache.match(event.request.url);
-                  }));
-
-              }else {
-                  console.log(`ServiceWorker Fetch: The resource ${event.request.url} is not in cache`);
-                  return;
-              }
-
-            }
-          }
-        ).catch( () => {
-          console.log(`ServiceWorker Fetch: Exception in has`);
-          }
-        );
-  
+  // Is this one of our precached assets?
+  const url = new URL(event.request.url);
+  const isPrecachedRequest = CACHE_ASSETS.includes(url.pathname);
+  if (isPrecachedRequest) {
+    // Grab the precached asset from the cache
+    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request.url);
+    }));
+  } else {
+    // Go to the network
+    return;
+  }  
 });
